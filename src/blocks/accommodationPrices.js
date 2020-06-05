4c74e90f91ab36b8a044d0f1abf3f2ee306804b8
Ritchie Vincent <ritchie@Ritchies-MacBook-Pro.local>
2020-06-05 14:09:11 +0100

    Reduce width of price tables, remove scrollbar on windows

--- src/blocks/accommodationPrices.js
+++ src/blocks/accommodationPrices.js
@@ -31,7 +31,8 @@ const Table = styled.div`
   box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2),
     0 1px 5px 0 rgba(0, 0, 0, 0.12);
   padding: 30px 15px;
-  overflow-x: scroll;
+  overflow-x: auto;
+  max-width: 400px;
 
   &::after {
     position: absolute;
@@ -41,6 +42,10 @@ const Table = styled.div`
     display: block;
     content: "Scroll \u2192";
     font-size: 0.7rem;
+  }
+
+  @media (min-width: 600px) {
+    max-width: 480px;
   }
 
   @media (min-width: 400px) {
--- src/blocks/priceTable.js
+++ src/blocks/priceTable.js
@@ -30,6 +30,7 @@ const PriceBlock = styled.div`
   box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2),
     0 1px 5px 0 rgba(0, 0, 0, 0.12);
   padding: 30px 30px 30px 30px;
+  max-width: 480px;
 
   &:not(:last-of-type) {
     margin-bottom: 40px;
